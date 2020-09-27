40f58b2429207ccf62dfabdb293d02f1dbb0a441
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2020-09-27 11:14:33 -0700

    Update rfd2.js

--- rfd2.js
+++ rfd2.js
@@ -4,7 +4,7 @@ function FetchData(tkn){
   $.ajax({
         async:false,
         type: "GET",
-        url:"https://a35c5feb0638.ngrok.io/FetchData",
+        url:"https://4e6ccb370e5e.ngrok.io/FetchData",
         data:{tkn}
         , 
         success: function(data) {
@@ -319,7 +319,7 @@ function sendplaceToServer(pos){
    $.ajax({
  async:false,
        type: "POST",
-        url:"https://a35c5feb0638.ngrok.io/UpdatePosition",
+        url:"https://4e6ccb370e5e.ngrok.io/UpdatePosition",
        data:{'data' :data,'tkn':tkn,'pageTarget':pageTarget},
         success: function(data) {
         if (data) {
