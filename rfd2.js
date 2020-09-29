18a4b01a8b9286478d13f1906935520909d83d19
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2020-09-29 05:31:01 -0700

    Update rfd2.js

--- rfd2.js
+++ rfd2.js
@@ -216,19 +216,20 @@ console.log("thi  "+reda.className)
 elementpicked= er;
 jQuery('body').bind('click.' + self.opts.namespace, clickHandler);
       $('.deferent').click(  function() {
-alert('here')
         var re= document.getElementsByClassName("trust-seals-preview")
    if(re && re.length>0){
     for (var i =re.length-1; i >-1; i--) {
           re[i].remove()
-    }
-//console.log(re)
-   }
-          myDomOutline.start();
+    }    
+     
+     myDomOutline.start();
         $('#pos-editor-instruction-container').css("display" ,"block");
         $('#pos-editor-actions').css("display" ,"none");
         $('#trust-seals-content-div').css("opacity","0.5");
                          
+//console.log(re)
+   }
+     
                        
 
 });
