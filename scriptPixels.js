f91d462a6ba2a1781521581a3bb131136184146e
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2021-01-14 06:25:42 -0800

    Update scriptPixels.js
    version All Apps

--- scriptPixels.js
+++ scriptPixels.js
@@ -133,3 +133,74 @@
     }
   }
 
+  // start collection page
+  else if(pageURL.indexOf('/collections') > -1) {
+    if(pixels.length) {
+      var fbTrackCode = "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');";
+      $.each(pixels,function(i,val) {
+        var fbPixel = val;
+        showPixel += "fbq('init', '"+fbPixel+"');";
+        showImgPixel += "<img height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id="+fbPixel+"&ev=PageView&noscript=1'/>"; 
+      });
+      if(showPixel != '') {
+		// Pixel here 
+		$('head').append("<script>"+fbTrackCode+""+showPixel+"fbq('track', 'PageView');</script><noscript>"+showImgPixel+"</noscript>");
+        // Start On Checkout button click
+        $('body').on('click', '[name="checkout"]', function() {
+          ajaxCheckout($,cart_url,fbTrackCode,currency);
+        });
+        if($('[name="checkout"]').length == 0) {
+          $('body').on('click', 'form[action="/checkout"] [type="submit"], [href="/checkout"], .fastcheckout_buy_button', function(){
+            ajaxCheckout($,cart_url,fbTrackCode,currency);
+          });
+        }
+        // End On Checkout button click
+      }
+    }
+  }
+    // start other pages
+  else {
+    if(pixels.length) {
+     $.each(pixels,function(i,val) { 
+        var fbPixel = $(this).text();
+        showPixel += "fbq('init', '"+fbPixel+"');";
+        showImgPixel += "<img height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id="+fbPixel+"&ev=PageView&noscript=1'/>";
+      });
+
+      if(showPixel != '') {
+        var fbTrackCode = "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');";
+
+        $('head').append("<script>"+fbTrackCode+""+showPixel+"fbq('track', 'PageView');</script><noscript>"+showImgPixel+"</noscript>")
+
+        
+        $('body').on('click', 'form[action="/cart/add"] [type="submit"], form[action="/cart/add"] [type="button"]', function(e){
+            var _main = $(this).parents('form[action="/cart/add"]');
+            var proID = _main.attr('id').replace(/[^0-9\.]/g,'');
+            var priceArr = _main.find('[name="id"] option:selected').text().split('-');
+            var price = priceArr[priceArr.length - 1].replace(/[^0-9\.]/g,'');
+            var proName = _main.parents('.product-single__meta--wrapper').find('[itemprop="name"]').text();
+            if(price == '') {
+                var proID = _main.find('[name="id"]').attr('data-section');
+                var price = $('div[data-section-id="'+proID+'"] #ProductPrice-'+proID).attr('content');
+                var proName = $.trim($('div[data-section-id="'+proID+'"]').find('[itemprop="name"]').text());
+            }
+            var showAddtoCartPixel = "fbq('track', 'AddToCart', {content_ids: ["+proID+"],content_type:'product_group',value: "+price+", content_name: '"+proName+"', currency: '"+currency+"', content_category: ''});";
+            if(showAddtoCartPixel != '' ) {
+                $('head').append("<script>"+fbTrackCode+""+showAddtoCartPixel+"</script>");
+            }
+        });
+        
+
+        // Start On Checkout button click
+        $('body').on('click', '[name="checkout"]', function() {
+          ajaxCheckout($,cart_url,fbTrackCode,currency);
+        });
+        if($('[name="checkout"]').length == 0) {
+          $('body').on('click', 'form[action="/checkout"] [type="submit"], [href="/checkout"], .fastcheckout_buy_button', function(){
+            ajaxCheckout($,cart_url,fbTrackCode,currency);
+          });
+        }
+        // End On Checkout button click
+      }
+    }
+  }
