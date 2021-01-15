7c4918b4a07ed8e5d9f229fb1e3c378c2b4acd09
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2021-01-15 15:34:36 -0800

    Update script14.js

--- script14.js
+++ script14.js
@@ -106,6 +106,11 @@ console.log(pixels)
                           showAddtoCartPixel += "fbq('track', 'AddToCart', {content_ids: ["+product.id+"],content_type:'product_group',value: "+price+", content_name: '"+product.title+"', currency: '"+currency+"', content_category: ''});";
                         }
                     });
+
+                                         if (pinterestid) {
+        pintrk('track', 'addtocart',  {content_ids: ["+product.id+"],content_type:'product_group',value: "+price+", content_name: '"+product.title+"', currency: '"+currency+"', content_category: ''});
+      }
+           
                     if(showAddtoCartPixel != '' ) {
                         $('head').append("<script>"+fbTrackCode+""+showAddtoCartPixel+"</script>");
                     }
