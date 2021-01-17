13d082dc2b90c5eb5ef54413840f812685cdcdab
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2021-01-16 16:14:41 -0800

    Update scriptPixels.js

--- scriptPixels.js
+++ scriptPixels.js
@@ -363,7 +363,7 @@ function loadtbpageview(tblid)
 {
 
 if (tblid) {
-var tblscripi = "window._tfa = window._tfa || [];window._tfa.push({notify: 'event', name: 'page_view', id: "+tblid+"});!function (t, f, a, x) { if (!document.getElementById(x)) { t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f); }}(document.createElement('script'),document.getElementsByTagName('script')[0], '//cdn.taboola.com/libtrc/unip/1259987/tfa.js','tb_tfa_script');"
+var tblscripi = "<script>window._tfa = window._tfa || [];window._tfa.push({notify: 'event', name: 'page_view', id: "+tblid+"});!function (t, f, a, x) { if (!document.getElementById(x)) { t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f); }}(document.createElement('script'),document.getElementsByTagName('script')[0], '//cdn.taboola.com/libtrc/unip/1259987/tfa.js','tb_tfa_script');</script>"
 $('head').append(tblscripi+"<noscript><img src='https://trc.taboola.com/"+tblid+"/log/3/unip?en=page_view'width='0' height='0' style='display:none' /></noscript>");
 
 }
